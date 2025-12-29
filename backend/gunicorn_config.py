52f040f35117538f6fcb5712acb5e6c929be0b23
Nik <SavenkoNikita@yandex.com>
2025-12-29 15:21:01 +0300

    подготовка к деплою завершена

--- backend/gunicorn_config.py
+++ backend/gunicorn_config.py
@@ -1,9 +1,16 @@
 import multiprocessing
 
-bind = "0.0.0.0:8000"
+bind = "127.0.0.1:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = "sync"
 timeout = 120
 keepalive = 5
 max_requests = 1000
 max_requests_jitter = 50
+
+accesslog = "-"  # stdout
+errorlog = "-"   # stdout
+loglevel = "info"
+
+preload_app = True
+reload = False
